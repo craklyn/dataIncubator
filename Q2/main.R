83ac1828b581b17a0b4be2f5180d7cc505050551
Daniel Blackburn <craklyn@gmail.com>
2017-07-19 22:29:51 -0700

    Commit minor changes

---
+++ Q2/main.R
@@ -0,0 +1,7 @@
+setwd('~/dataIncubator/Q2/Data')
+
+download.file('https://resources.lendingclub.com/LoanStats3c.csv.zip', dest='Data/LoanStats3c.csv.zip') 
+unzip('Data/LoanStats3c.csv.zip')
+download.file('https://resources.lendingclub.com/LoanStats3d.csv.zip', dest='Data/LoanStats3d.csv.zip')
+unzip('Data/LoanStats3d.csv.zip')              
+
