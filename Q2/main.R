a01cd69d26096c099186c889df330a27c6758284
Daniel Blackburn <craklyn@gmail.com>
2017-07-21 16:02:06 -0700

    In principle, final answers to Q2.  Of course, I am sure I will revisit..."

--- Q2/main.R
+++ Q2/main.R
@@ -1,4 +1,5 @@
 options(digits=10)
+library(dplyr)
 setwd('~/dataIncubator/Q2')
 
 if(!file.exists("Data")) {
@@ -67,12 +68,11 @@ print(minIntRate$meanInterestRate / maxIntRate$meanInterestRate)
 
 
 ## Q4  ##
-print("What is the difference in the fraction of the loans with a 36-month 
-      term between 2014 and 2015?")
+cat("What is the difference in the fraction of the loans with a 36-month 
+term between 2014 and 2015?")
 
 loanData$issue_year <- sub(".*-", "", as.character(loanData$issue_d))
 loanData$issue_month <- sub("-.*", "", as.character(loanData$issue_d))
-
 
 tbl_df(loanData) %>% 
   group_by(issue_year, term) %>% 
@@ -96,15 +96,30 @@ between the last payment date and the issue date, divided by the term
 of loan. What is the standard deviation of this ratio for all the 
 loans in default?")
 
-as.monyear(loanData$issue_d)
-as.mon
+monthsIntoPeriod <-
+  c("Jan-2014"=1, "Feb-2014"=2, "Mar-2014"=3, "Apr-2014"=4, "May-2014"=5, "Jun-2014"=6, 
+    "Jul-2014"=7, "Aug-2014"=8, "Sep-2014"=9, "Oct-2014"=10, "Nov-2014"=11, "Dec-2014"=12,
+    "Jan-2015"=13, "Feb-2015"=14, "Mar-2015"=15, "Apr-2015"=16, "May-2015"=17, "Jun-2015"=18, 
+    "Jul-2015"=19, "Aug-2015"=20, "Sep-2015"=21, "Oct-2015"=22, "Nov-2015"=23, "Dec-2015"=24,
+    "Jan-2016"=25, "Feb-2016"=26, "Mar-2016"=27, "Apr-2016"=28, "May-2016"=29, "Jun-2016"=30, 
+    "Jul-2016"=31, "Aug-2016"=32, "Sep-2016"=33, "Oct-2016"=34, "Nov-2016"=35, "Dec-2016"=36,
+    "Jan-2017"=37, "Feb-2017"=38, "Mar-2017"=39, "Apr-2017"=40, "May-2017"=41, "Jun-2017"=42, 
+    "Jul-2017"=43, "Aug-2017"=44, "Sep-2017"=45, "Oct-2017"=46, "Nov-2017"=47, "Dec-2017"=48)
+loanPeriod <- c(" 36 months"=36, " 60 months"=60)
+
+loanData$issue_d_monthsInto2014 <- monthsIntoPeriod[as.character(loanData$issue_d)]
+loanData$last_pymnt_d_monthsInto2014 <- monthsIntoPeriod[as.character(loanData$last_pymnt_d)]
+loanData$term_inMonths <- loanPeriod[as.character(loanData$term)]
 
 inDefaultLoanData <- subset(loanData, 
                           !(loan_status %in% c("Fully Paid", 
                                              "Current", 
                                              "In Grace Period")))
 
-inDefaultLoanData$last_pymnt_d - inDefaultLoanData$issue_d
+#TODO: Understand if I should be removing NA's
+sd((inDefaultLoanData$last_pymnt_d_monthsInto2014 
+    - inDefaultLoanData$issue_d_monthsInto2014) / inDefaultLoanData$term_inMonths,
+   na.rm=TRUE)
 
 
 ## Q6  ##
@@ -113,18 +128,39 @@ of return, as figured from the total payments and the loan amount, and
 the interest rate? Consider only loans that have reached the end of 
 their term.")
 
-levels(loanData$loan_status)
+endTermLoanData <- subset(loanData, loan_status == "Fully Paid")
 
-#cor(loanData, method="pearson")
-
+cor(x = (endTermLoanData$total_pymnt - endTermLoanData$loan_amnt),
+    y= endTermLoanData$int_rate, 
+    method="pearson")
 
 ## Q7  ##
 print("Let's find a loan purpose that shows up abnormally often in one 
-      state. Call AA the probability of a loan going to a specific purpose 
-      nationwide. Call BB the probability of a loan going to a specific 
+      state. Call A the probability of a loan going to a specific purpose 
+      nationwide. Call B the probability of a loan going to a specific 
       purpose for each state. Out of all (state, purpose) pairs with at 
       least 10 loans, what is the highest ratio of B / A (i.e. the most 
       surprising)?")
+
+A <- tbl_df(loanData) %>% 
+  group_by(purpose) %>% 
+  summarise (n = n()) %>%
+  mutate(purpose_freq = n / sum(n))
+
+B <- tbl_df(loanData) %>% 
+  group_by(addr_state, purpose) %>% 
+  summarise (n = n()) %>%
+  mutate(bystate_purpose_freq = n / sum(n))
+A[['n']] <- NULL
+B[['n']] <- NULL
+AB <- merge(A,B)
+AB <- AB %>%
+  mutate(surprisingness = bystate_purpose_freq/purpose_freq) %>%
+  arrange(-surprisingness)
+
+head(AB)
+AB$surprisingness[1]
+
 
 ## Q8  ##
 print("Group all loans by their sub-grade and calculate their average 
@@ -132,6 +168,20 @@ print("Group all loans by their sub-grade and calculate their average
       the (average) default rate from the (average) interest rate. Find that 
       sub-grade with the largest absolute deviation from this model. What is 
       the deviation of the actual default rate from the predicted value?")
+
+loanData$inDefault <- !(loanData$loan_status %in% c("Fully Paid", 
+                                                    "Current", 
+                                                    "In Grace Period"))
+
+xy <- tbl_df(loanData) %>% 
+  group_by(sub_grade) %>% 
+  summarize(meanInterestRate = mean(int_rate), fractionInDefault = mean(inDefault))
+lmfit <- lm(fractionInDefault~meanInterestRate, xy)
+max(abs(residuals(lmfit)))
+
+plot(fractionInDefault~meanInterestRate, xy)
+abline(lmfit, col='red')
+
 
 ## Q9  ##
 print("Please provide the script used to generate this result (max 10000 characters).")
